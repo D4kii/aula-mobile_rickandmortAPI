f6cfd302b268644e97799d1a70090c01c491fbf4
DANIELA LINO DA SILVA OLIVEIRA <22282183@dsn127m1242267.cfp127aluno.local>
2023-05-09 16:57:23 -0300

    introduzindo a API

--- app/src/main/java/br/senai/sp/jandira/rickandmorty/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/rickandmorty/MainActivity.kt
@@ -3,22 +3,26 @@ package br.senai.sp.jandira.rickandmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.rickandmorty.model.Character
 import br.senai.sp.jandira.rickandmorty.model.CharacterList
+import br.senai.sp.jandira.rickandmorty.model.Info
 import br.senai.sp.jandira.rickandmorty.service.RetrofitFactory
 import br.senai.sp.jandira.rickandmorty.ui.theme.RickAndMortyTheme
+import coil.compose.AsyncImage
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -44,11 +48,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
 
-    var listCharacter = remember {
-        mutableListOf(listOf<Character>())
+    val listCharacter = remember {
+        mutableStateOf(listOf<br.senai.sp.jandira.rickandmorty.model.Character>())
+    }
+    val info by remember {
+        mutableStateOf(Info())
     }
 
-    Column() {
+    Column(modifier = Modifier.padding(16.dp)) {
         Button(onClick = {
             //Cria uma chamada para o endpoint
             val call = RetrofitFactory().getCharacterService().getCharacter()
@@ -59,7 +66,8 @@ fun Greeting(name: String) {
                     call: Call<CharacterList>,
                     response: Response<CharacterList>
                 ) {
-                    TODO("Not yet implemented")
+                    listCharacter.value = response.body()!!.results
+                    val teste = response.body()!!.info
                 }
 
                 override fun onFailure(call: Call<CharacterList>, t: Throwable) {
@@ -69,23 +77,55 @@ fun Greeting(name: String) {
             })
 
         }) {
-            Text(text = "Listar Personagens")
+            Text(
+                text = "Listar Personagens")
         }
+        Row() {
+            Text(
+                text = "Count",
+            modifier = Modifier.size(width = 60.dp, height = 20.dp)
+            )
+            Text(
+                text = "400",
+                modifier = Modifier.size(width = 60.dp, height = 20.dp),
+                textAlign = TextAlign.End
+            )
+        }
+        Row() {
+            Text(
+                text = "Pages",
+                modifier = Modifier.size(width = 60.dp, height = 20.dp)
+            )
+            Text(
+                text = "${info.pages}",
+                modifier = Modifier.size(width = 60.dp, height = 20.dp),
+                textAlign = TextAlign.End
+            )
+        }
+
         LazyColumn(){
-            items(10){
+            items(listCharacter.value){
                 Card(
                     backgroundColor = Color.Magenta,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp, vertical = 4.dp)
                 ) {
+
+                    Row() {
+                        AsyncImage(
+                            model = it.image,
+                            contentDescription = "Character avatar")
+                    }
                     Column(
                         modifier = Modifier.padding(8.dp)
                     ) {
-                        Text(text = "Espécie do personagem")
-                        Text(text = "Nome do personagem")
+                        Text(text = it.name,
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(text = it.species)
                     }
-
                 }
             }
         }
--- app/src/main/java/br/senai/sp/jandira/rickandmorty/service/CharacterService.kt
+++ app/src/main/java/br/senai/sp/jandira/rickandmorty/service/CharacterService.kt
@@ -4,13 +4,24 @@ import br.senai.sp.jandira.rickandmorty.model.CharacterList
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CharacterService {
 
-    @GET("Character")
+    @GET("character")
     fun getCharacter(): Call<CharacterList>
 
     @GET("character/{id}")
     fun getCharacterById(@Path("id") id: Long): Call<Character>
 
+    @GET("character/")
+    fun getCharacterByPage(@Query("page") pageNumber: Int): Call<CharacterList>
+
+    //https://rickandmortyapi.com/api/character/?name=rick&status=alive
+
+    @GET("character/")
+    fun getCharacterByNameAndStatus(
+       @Query("name") name: String,
+       @Query("status") status: String
+    ): Call<CharacterList>
 }
